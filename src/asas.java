d66a281ce9b2477d841308ac906f8ad1a8b37e38
tonyxjk <2789293955@qq.com>
2022-10-28 21:25:18 +0800

    变更

--- src/asas.java
+++ src/asas.java
@@ -4,5 +4,6 @@ public class asas {
         System.out.println("112231");
         System.out.println("1121212");
         System.out.println("122121");
+        System.out.println("212121");
     }
 }
